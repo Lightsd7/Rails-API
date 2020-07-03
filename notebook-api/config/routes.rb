f3dc6854372be1bc54d346ac7d286e72981af78e
Raphael Molina <raphael.bedran@konduto.com>
2020-07-03 09:27:35 -0300

    Versionamento
    Aplicando versionamento no sistema

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,16 +1,33 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :auths, only: [:create]
+
+  # resources :auths, only: [:create]
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show, :update, :create, :destroy]
-    resource :phones, only: [:show, :update, :create, :destroy], path: 'relationships/phones'
+  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1.0"}) do
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      resource :phones, only: [:show, :update, :create, :destroy]
+      resource :phones, only: [:show, :update, :create, :destroy], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+
+  api_version(:module => "V2", :header => {:name => "X-Version", :value => "2.0"}) do
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show, :update, :create, :destroy]
+      resource :phones, only: [:show, :update, :create, :destroy], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
   end
   # get '/contacts', to: "contacts#index"
   
